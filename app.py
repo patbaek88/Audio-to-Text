12cc15e5036a8f99e61c28bd3dcec2b44cfb8343
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-12-22 16:28:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,23 @@ import soundfile as sf
 st.title('Audio to Text')  # 타이틀명 지정
 st.write("")
 
-filename = st.file_uploader('녹음파일(m4a)을 업로드 하세요.', type=['m4a'])
+audio_m4a = st.file_uploader('녹음파일(m4a)을 업로드 하세요.', type=['m4a'])
 
 
 #filename = input("Text로 변환하려고하는 녹음파일(m4a)의 이름을 입력하세요  \n")    
+
+
+#filename = input("Text로 변환하려고하는 녹음파일(m4a)의 이름을 입력하세요  \n")    
+
+# m4a 파일 경로
+path_m4a = audio_m4a
+
+# wav 파일 경로
+path_wav = converted+".wav"
+
+# m4a 파일 로드
+#audio_m4a = AudioSegment.from_file(path_m4a, format="m4a")
+
+# wav 파일로 변환
+audio_wav = audio_m4a.export(path_wav, format="wav")
+
